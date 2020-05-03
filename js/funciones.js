cddbe24e042fc112fda1b270263c08ee30a61f08
Alwar <alwaromoreno@gmail.com>
2020-05-03 19:46:47 +0200

    Funcionalidades
    Añadir, eliminar, filtrar y buscar tareas

---
+++ js/funciones.js
@@ -0,0 +1,136 @@
+
+
+// 1. Pintar tareas en el HTML 
+
+
+var seccionTareas = document.getElementById('seccionTareas');
+
+function pintarTareas(pListaTareas) {
+
+    seccionTareas.innerHTML = "";
+
+    pListaTareas.forEach(tarea => {
+        seccionTareas.innerHTML +=
+            ` <article class="tarea">
+                                    <p>${tarea.titulo}</p>
+                                    <div class="eliminar" class="">
+                                        <i class="fas fa-trash"></i>
+                                    </div>
+                                </article>`;
+
+
+    });
+
+    let colorEliminar = document.getElementsByClassName('eliminar');
+    let idTarea = document.getElementsByClassName('tarea');
+
+    for (let i = 0; i < pListaTareas.length; i++) {
+        let importancia = pListaTareas[i]['importancia'];
+        let id = pListaTareas[i]['idTarea'];
+
+        colorEliminar[i].classList.add(importancia);
+        idTarea[i].setAttribute('id', id);
+    }
+
+
+    // Recogemos la clase de borrado cada vez que pintamos para que no queden tareas exluídas.
+    var basuras = document.getElementsByClassName('fa-trash');
+
+    for (basura of basuras) {
+        basura.addEventListener('click', eliminarTarea);
+    }
+}
+pintarTareas(listaTareas);
+
+
+// 2. Eliminar tarea. Nótese que recolectamos los elementos a borrar cada vez que pintamos las tareas para que las nuevas no queden excluidas
+
+
+function eliminarTarea(event) {
+    // a) borramos del Array
+    let idTarea = event.target.parentNode.parentNode.getAttribute('id');
+    let tareaBorradaLista = listaTareas.findIndex(tarea => tarea.idTarea == idTarea);
+    listaTareas.splice(tareaBorradaLista, 1);
+
+    // b) borramos del HTML
+    event.target.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode);
+}
+
+
+// 3. Añadir tarea
+
+
+
+var btAnadir = document.getElementById('btAnadir');
+btAnadir.addEventListener('click', anadirTarea);
+
+function anadirTarea(event) {
+    event.preventDefault();
+
+    let importancia = document.getElementById('importancia').value;
+    let nuevaTarea = document.getElementById('nuevaTarea').value;
+
+    if (importancia != 'queimportancia' && nuevaTarea != "") {
+        let tarea = {
+            idTarea: listaTareas[listaTareas.length - 1]['idTarea'] + 1,
+            titulo: nuevaTarea,
+            importancia: importancia
+        }
+        listaTareas.push(tarea);
+    }
+
+    pintarTareas(listaTareas);
+
+}
+
+
+
+
+
+// 4. Filtrar tarea
+
+var btFiltrar = document.getElementById('btFiltrar');
+btFiltrar.addEventListener('click', filtrarImportancia);
+
+function filtrarImportancia(event) {
+    event.preventDefault();
+
+    let tipoImportancia = document.getElementById('tipoImportancia').value;
+    let listaFiltro = new Array();
+
+    for (tarea of listaTareas) {
+        if (tipoImportancia == tarea.importancia) {
+            listaFiltro.push(tarea);
+        }
+    }
+    pintarTareas(listaFiltro);
+}
+
+
+
+
+// 5. Buscar tarea
+
+var btBuscar = document.getElementById('btBuscar');
+btBuscar.addEventListener('click', buscarTarea);
+
+function buscarTarea() {
+    event.preventDefault();
+
+    busqueda = document.getElementById('busqueda').value.toLowerCase();
+    let busquedaPersonalizada = new Array();
+
+    for (tarea of listaTareas) {
+        if (tarea.titulo.toLowerCase().includes(busqueda)) {
+            busquedaPersonalizada.push(tarea);
+        }
+    }
+    pintarTareas(busquedaPersonalizada);
+
+}
+
+
+
+
+
+
---
+++ js/lista.js
@@ -0,0 +1,34 @@
+// 1. Lista de tareas.
+
+const listaTareas = new Array(
+    {
+        'idTarea': 0,
+        'titulo': 'Estudiar javascript',
+        'importancia':'urgente'
+    },
+    {
+        'idTarea': 1,
+        'titulo': 'Hacer el Camino de Santiago',
+        'importancia':'urgente'
+    },
+    {
+        'idTarea': 2,
+        'titulo': 'Pegarse un tiro en la boca',
+        'importancia':'urgente'
+    },
+    {
+        'idTarea': 3,
+        'titulo': '1000 slavic squads, 1500 flexiones, 5000 abs',
+        'importancia':'semanal'
+    },
+    {
+        'idTarea': 4,
+        'titulo': 'Vol. IV Summa Theologiae',
+        'importancia':'semanal'
+    },
+    {
+        'idTarea': 5,
+        'titulo': 'Quemar las calles: 15 de Mayo, revolución de los patos',
+        'importancia':'mensual'
+    }
+);
